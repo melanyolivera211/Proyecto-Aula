83c32fe58858601f457c5a35dfe7f2171dda0cb2
melanyolivera211 <143230796+melanyolivera211@users.noreply.github.com>
2023-11-01 20:13:07 -0500

    Delete ProyectoAula directory

--- ProyectoAula/src/proyectoaula/objects/Electrodomesticos.java
+++
@@ -1,8 +0,0 @@
-package proyectoaula.objects;
-public class Electrodomesticos {
-    public String nombreE; 
-    public String nroserie;
-    public String marca;
-    public Usuario usuario; 
-    public Gastos gasto;
-}
