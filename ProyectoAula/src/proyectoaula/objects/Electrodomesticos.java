8c8ae3c00fa2f726aa23b5d7a1616e2928680497
camilodangaud <143123036+camilodangaud@users.noreply.github.com>
2023-11-01 20:15:20 -0500

    camilo

---
+++ ProyectoAula/src/proyectoaula/objects/Electrodomesticos.java
@@ -0,0 +1,8 @@
+package proyectoaula.objects;
+public class Electrodomesticos {
+    public String nombreE; 
+    public String nroserie;
+    public String marca;
+    public Usuario usuario; 
+    public Gastos gasto;
+}
